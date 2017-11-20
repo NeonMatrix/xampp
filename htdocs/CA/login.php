995e0b6edce9941079654f93cf117fca65f7e757
NeonMatrix <povkub@gmail.com>
2017-11-20 23:23:30 +0000

    web dev CA

---
+++ htdocs/CA/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Library Login</title>
+</head>
+<body>
+
+<form method="post">
+	<p>Pame:
+	<input type="text" name="pname"></p>
+</form>
+
+</body>
+</html>
